cc52b2853fc4c78d2899eed0df4f9ce3111c3b91
Oskar Kobierecki <oskar.kobierecki@o2.pl>
2018-01-30 18:56:26 +0100

    general cleanup

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-var operators = ['/', '*', '-', '+'];
-var levelDisplay = 'level ' + (levelNumber - 1);
-levelDisplay = levelDisplay.split(' ');
-var levelNumber = 2;
-
+(function(){
 
 function randomNumberRange(min, max) {
 	return Math.floor(Math.random() * (max - min) + min);
@@ -20,54 +16,59 @@ var Equation = function(left, right, operator) {
 		} else {
 			return '(' + left + ' ' + operator + ' ' + right + ')';
 		}
-	}
+	}	
+}
 
+//GETTING THE VALUE, COMPERING, INCREASING LEVEL
 
-	//GETTING THE VALUE, COMPERING, INCREASING LEVEL
-	self = this;
-
+function inputListener(equationResult){
 	$('#value').keypress(function(event) {
-		var value = parseInt($("#value").val());
+		var value = parseFloat($("#value").val());
 
 		var keycode = (event.keyCode ? event.keyCode : event.which);
-		if (keycode == '13' && value == self.expression) {
-			console.log('enter working');
+		if (keycode == '13' && value == equationResult) {
+			console.log('enter works');
 			// alert('u a good at math');
-			$('#output').text(generateEquation(levelNumber).toString());
 			$("#value").val('');
-			levelNumber +=1;
-			console.log('level number: ' + levelNumber);
-
-
-			//ADDING POINTS
-			var points = parseInt($('#points').text());
-			var updatedPoints = points + 2;
-			$('#points').text(updatedPoints);
+			levelCounter();
+			pointsResume();	
+			gameMaster();
 		}
 	});
 }
 
+function levelCounter(){
+	sublevel ++;
+	if(sublevel == 5){
+		levelNumber ++;
+		sublevel = 1;
+	} 
+	console.log('level number: ' + levelNumber);
+	console.log('sublevel: ' + sublevel);
+}
+
+//ADDING POINTS
+
+function pointsResume(){
+	var sublevel = 1;
+	var points = parseInt($('#points').text());
+	points += 1;
+	$('#points').text(points);
+}
 
 function generateEquation(numNodes) {
 	if (numNodes === 1) return randomNumberRange(1, 10);
 
-	var randomBracket = randomNumberRange(1, 3);
-
-	var numLeft = randomBracket === 1 ? Math.floor(numNodes / 2) : Math.ceil(numNodes / 2);
+	var randomBrackets = randomNumberRange(1, 3);
+	var numLeft = randomBrackets === 1 ? Math.floor(numNodes / 2) : Math.ceil(numNodes / 2);
 	var leftSubTree = generateEquation(numLeft);
-	var numRight = randomBracket === 1 ? Math.ceil(numNodes / 2) : Math.floor(numNodes / 2);
+	var numRight = randomBrackets === 1 ? Math.ceil(numNodes / 2) : Math.floor(numNodes / 2);
 	var rightSubTree = generateEquation(numRight);
 
 	var randomOperator = randomNumberRange(0, operators.length);
 	var sign = operators[randomOperator];
 	return new Equation(leftSubTree, rightSubTree, sign);
 }
-
-var equationGenerate = generateEquation(levelNumber);
-var equationDisplay = $('#output').text(equationGenerate).toString();
-var equationResult = equationGenerate.getExpression();
-console.log(equationResult);
-
 
 //TIMER
 
@@ -92,6 +93,22 @@ function startTimer(duration) {
 	}, 1000);
 }
 
-var timeToStop = 3000000000000000000000000000000000000;
+	var operators = ['/', '*', '-', '+'];
+	var levelDisplay = 'level: ' + (levelNumber - 1);
+	var levelNumber = 2;
+	var sublevel = 1;
 
-startTimer(timeToStop);
+	function gameMaster(){
+
+		var equationGenerate = generateEquation(levelNumber);
+		var equationDisplay = $('#output').text(equationGenerate).toString();
+		var equationResult = equationGenerate.getExpression();
+		console.log(equationResult);
+		inputListener(equationResult);
+
+		var timeToStop = 60;
+		startTimer(timeToStop);
+	}
+gameMaster();
+})();
+
